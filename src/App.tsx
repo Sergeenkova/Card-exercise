2742967002a06bbdd0bc631968de6819156a0025
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2026-01-05 12:10:47 +0100

    Card exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import cardImage from './assets/images/image.svg';
+import { myTheme } from './styles/Theme.Styled';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledBox>
+        <StyledCard>
+          <StyledImage src={cardImage} alt="Card image" />
+          <StyledTitle>Headline</StyledTitle>
+          <StyledParagraph>
+            Faucibus. Faucibus. Sit sit sapien sit<br></br>tempusrisu ut. Sit
+            molestie ornare in venen.
+          </StyledParagraph>
+          <StyledBoxBtn>
+            <StyledBtnInfo>See more</StyledBtnInfo>
+            <StyledBtnSave color={"#4E71FE"} backgroundColor={"transparent"}>Save</StyledBtnSave>
+          </StyledBoxBtn>
+        </StyledCard>
+      </StyledBox>
     </div>
   );
 }
 
 export default App;
+
+const StyledBox = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledCard = styled.div`
+  font-family: ${myTheme.fonts.family};
+  background-color: ${myTheme.colors.colorSecondary};
+  padding: 10px 10px 0px;
+  width: 300px;
+  height: 350px;
+  border-radius: 15px;
+  box-shadow: 0px 4px 20px 5px rgba(0, 0, 0, 0.1);
+`
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+`
+const StyledTitle = styled.h1`
+  font-size: ${myTheme.title.fontSize};
+  font-weight: ${myTheme.title.fontWeight};
+  color: ${myTheme.colors.textPrimary};
+  padding: 15px 0 0 10px;
+`;
+
+const StyledParagraph = styled.p`
+  font-size: ${myTheme.paragraph.fontSize};
+  font-weight: ${myTheme.paragraph.fontWeight};
+  color: ${myTheme.colors.textSecondary};
+  padding: 20px 0 0 10px;
+  line-height: 1.66667;
+`
+
+const StyledBoxBtn = styled(StyledBox)`
+  height: auto;
+  justify-content: start;
+  gap: 12px;
+  padding: 18px 10px;
+
+  button {
+    cursor: pointer;
+  }
+`
+
+const StyledBtnInfo = styled.button`
+  border: none;
+  border-radius: 5px;
+  background-color: ${myTheme.colors.colorPrimary};
+  color: ${myTheme.colors.colorSecondary};
+  font-size: ${myTheme.button.fontSize};;
+  font-weight: ${myTheme.button.fontWeight};
+  padding: 10px 20px;
+`;
+
+type StyledBtnSavePropsType = {
+  color: string;
+  backgroundColor: string;
+}
+
+const StyledBtnSave = styled(StyledBtnInfo)<StyledBtnSavePropsType>`
+  border: 2px solid ${props => props.color};
+  background-color: ${props => props.backgroundColor};
+  color: ${props => props.color};
+  padding: 8px 29px;
+  `
---
+++ src/styles/Theme.Styled.tsx
@@ -0,0 +1,27 @@
+export const myTheme = {
+  fonts: {
+    family: "'Inter', sans-serif",
+  },
+
+  colors: {
+    textPrimary: "#000000",
+    textSecondary: "#abb3ba",
+    colorPrimary: "#4E71FE",
+    colorSecondary: "#ffffff",
+  },
+
+   title: {
+      fontSize: "16px",
+      fontWeight: 700,
+    },
+
+    paragraph: {
+      fontSize: "12px",
+      fontWeight: 500,
+    },
+
+    button: {
+      fontSize: "10px",
+      fontWeight: 700,
+    },
+};
